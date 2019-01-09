471b894b36c9f9bf819c67547d55fabdb7fce43c
Dawid <dawid.deby@gmail.com>
2019-01-09 22:08:30 +0100

    Item.vue & Modal.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div :class="[{ flexStart: step === 1 }, 'wrapper']">
         <transition name="slide">
-                <img src="./assets/logo.png" class="logo" v-if="step === 1">
+            <img src="./assets/logo.png" class="logo" v-if="step === 1">
         </transition>
         <transition name="fade">
             <HeroImage v-if="step === 0"/>
@@ -9,8 +9,10 @@
         <Claim v-if="step === 0" />
         <SearchInput v-model="searchValue" @input="handleInput" :dark="step === 1"/>
         <div class="results" v-if="results && !loading && step === 1">
-            <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id" />
+            <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id" @click.native="handleModalOpen(item)"/>
         </div>
+        <div class="loader" v-if="step === 1 && loading" />
+        <Modal v-if="modalOpen" :item="modalItem" @closeModal="modalOpen = false"/>
     </div>
 </template>
 
@@ -20,6 +22,7 @@ import Claim from '@/components/Claim.vue';
 import HeroImage from '@/components/HeroImage.vue';
 import SearchInput from '@/components/SearchInput.vue';
 import Item from '@/components/Item.vue';
+import Modal from '@/components/Modal.vue';
 
 import axios from 'axios';
 import debounce from 'lodash.debounce';
@@ -30,6 +33,8 @@ export default {
     name: 'App',
     data() {
         return {
+            modalOpen: false,
+            modalItem: null,
             loading: false,
             step: 0,
             searchValue: '',
@@ -41,8 +46,13 @@ export default {
         SearchInput,
         HeroImage,
         Item,
+        Modal,
     },
     methods: {
+        handleModalOpen(item){
+            this.modalOpen = true;
+            this.modalItem = item;
+        },
 
         // eslint-disable-next-line
         handleInput: debounce(function(){
@@ -120,14 +130,14 @@ body {
     transition: margin-top .3s ease;
 }
 .slide-enter, .slide-leave-to{
-margin-top: -100px;
+    margin-top: -100px;
 }
 
 .fade-enter-active, .fade-leave-active {
     transition: opacity .3s ease;
 }
 .fade-enter, .fade-leave-to{
-opacity: 0;
+    opacity: 0;
 }
 
 
@@ -149,5 +159,38 @@ opacity: 0;
     }
 
 }
+
+.loader {
+    margin-top: 100px;
+    display: inline-block;
+    width: 64px;
+    height: 64px;
+
+    @media(min-width: 768px){
+        width: 90px;
+        height: 90px;
+    }
+}
+.loader:after {
+    content: " ";
+    display: block;
+    width: 46px;
+    height: 46px;
+    margin: 1px;
+    border-radius: 50%;
+    border: 5px solid #1e3d4a;
+    border-color: #1e3d4a transparent #1e3d4a transparent;
+    animation: loading 1.2s linear infinite;
+}
+@keyframes loading {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+
+
 
 </style>
