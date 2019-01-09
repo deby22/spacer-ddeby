2cd140d5eedb9de7b9927ea963d2fe8fc5ad9d2d
Dawid <dawid.deby@gmail.com>
2019-01-09 20:33:07 +0100

    Item component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
-    <div class="wrapper">
-    <HeroImage />
-    <Claim />
-    <SearchInput v-model="searchValue" @input="handleInput"/>
+    <div :class="[{ flexStart: step === 1 }, 'wrapper']">
+        <transition name="slide">
+                <img src="./assets/logo.png" class="logo" v-if="step === 1">
+        </transition>
+        <transition name="fade">
+            <HeroImage v-if="step === 0"/>
+        </transition>
+        <Claim v-if="step === 0" />
+        <SearchInput v-model="searchValue" @input="handleInput" :dark="step === 1"/>
+        <div class="results" v-if="results && !loading && step === 1">
+            <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id" />
+        </div>
     </div>
 </template>
 
@@ -11,6 +19,7 @@
 import Claim from '@/components/Claim.vue';
 import HeroImage from '@/components/HeroImage.vue';
 import SearchInput from '@/components/SearchInput.vue';
+import Item from '@/components/Item.vue';
 
 import axios from 'axios';
 import debounce from 'lodash.debounce';
@@ -18,32 +27,38 @@ import debounce from 'lodash.debounce';
 const API = 'https://images-api.nasa.gov/search';
 
 export default {
-  name: 'App',
-  data() {
-    return {
-      searchValue: '',
-      results: [],
-    };
-  },
-  components: {
-      Claim,
-      SearchInput,
-      HeroImage,
-  },
-  methods: {
+    name: 'App',
+    data() {
+        return {
+            loading: false,
+            step: 0,
+            searchValue: '',
+            results: [],
+        };
+    },
+    components: {
+        Claim,
+        SearchInput,
+        HeroImage,
+        Item,
+    },
+    methods: {
 
-    // eslint-disable-next-line
-    handleInput: debounce(function(){
-      console.log(this.searchValue);
-      axios.get(`${API}?q=${this.searchValue}&media_type=image`)
-        .then((response) => {
-          this.results = response.data.collection.items;
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }, 500),
-  },
+        // eslint-disable-next-line
+        handleInput: debounce(function(){
+            this.loading = true;
+            console.log(this.searchValue);
+            axios.get(`${API}?q=${this.searchValue}&media_type=image`)
+                .then((response) => {
+                    this.results = response.data.collection.items;
+                    this.loading = false;
+                    this.step = 1;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }, 500),
+    },
 };
 </script>
 
@@ -51,6 +66,7 @@ export default {
 <style lang="scss" scoped>
 .wrapper {
     margin: 0;
+    position: relative;
     width: 100%;
     min-height: 100vh;
     height: 100vh;
@@ -85,14 +101,53 @@ $font-weight-bold: 600;
 $font-weight-black: 800;
 
 *{
+    font-family: 'Montserrat', sans-serif;
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
 }
 
 body {
-    font-family: 'Montserrat', sans-serif;
     margin: 0;
     padding: 0;
 }
+
+.flexStart {
+    justify-content: flex-start;
+}
+
+.slide-enter-active, .slide-leave-active {
+    transition: margin-top .3s ease;
+}
+.slide-enter, .slide-leave-to{
+margin-top: -100px;
+}
+
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .3s ease;
+}
+.fade-enter, .fade-leave-to{
+opacity: 0;
+}
+
+
+
+.logo {
+    position: absolute;
+    top: 40px;
+}
+
+
+.results {
+    margin-top: 50px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+
+    @media(min-width: 768px){
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+
+}
+
 </style>
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -2,6 +2,7 @@
     <input 
     id="search"
     name="search"
+    :class="{ dark }"
     :value="value"
     @input="handleChange"
     />
@@ -15,6 +16,10 @@ export default {
         value: {
             type:String,
             required: true,
+        },
+        dark: {
+            type: Boolean,
+            default: false,
         },
     },
     methods: {
@@ -47,6 +52,15 @@ input {
     input:focus{
         outline: none;
         box-shadow: 0 10px 20px -8px rgba(255, 255, 255, .2);
+    }
+
+    .dark {
+        color: #1e3d4a;
+        border-bottom-color: #1e3d4a;
+    }
+
+    .dark:focus {
+        box-shadow: 0 10px 20 px -8px rgba(#1e3d4a, .2);
     }
 
 </style>
